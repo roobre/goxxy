821e3cab62e97bdd6e37d391821682e214552f88
Roberto Santalla <roobre@roobre.es>
2018-08-31 16:56:20 +0200

    First attempt at designing the Matcher API

--- goxxy.go
+++ goxxy.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 )
 
+var nopGoxxy = Goxxy{Client: http.DefaultClient}
+
 type Middleware interface {
 	Middleware(handler http.Handler) http.Handler
 }
@@ -24,11 +26,21 @@ func (mf ManglerFunc) Mangle(response *http.Response) *http.Response {
 	return mf(response)
 }
 
+type Matcher interface {
+	Match(r *http.Request) bool
+}
+type MatcherFunc func(r *http.Request) bool
+
+func (mf MatcherFunc) Match(r *http.Request) bool {
+	return mf(r)
+}
+
 type Goxxy struct {
 	Client      *http.Client
 	ErrHandler  http.Handler
 	middlewares []Middleware
 	manglers    []Mangler
+	matchers    []Matcher
 }
 
 func New() *Goxxy {
@@ -49,8 +61,28 @@ func (g *Goxxy) AddManglerFunc(mg ManglerFunc) {
 	g.manglers = append(g.manglers, mg)
 }
 
+func (g *Goxxy) Match(r *http.Request) http.Handler {
+	if len(g.matchers) == 0 {
+		return http.HandlerFunc(g.proxy)
+	}
+
+	for _, m := range g.matchers {
+		if m.Match(r) {
+			handler, isHandler := m.(http.Handler)
+			if isHandler {
+				return handler
+			} else {
+				return http.HandlerFunc(g.proxy)
+			}
+		}
+	}
+
+	return http.HandlerFunc(nopGoxxy.proxy)
+}
+
 func (g *Goxxy) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	var handler http.Handler = http.HandlerFunc(g.proxy)
+	handler := g.Match(r)
+
 	for i := len(g.middlewares) - 1; i >= 0; i-- {
 		handler = g.middlewares[i].Middleware(g)
 	}
