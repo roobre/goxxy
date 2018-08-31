7cee7c497f8645c85bf0124a8778767c13428247
Roberto Santalla <roobre@roobre.es>
2018-08-31 16:22:18 +0200

    Fixed passing -1 as cap if response size was unknown

--- modules/module.go
+++ modules/module.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 )
 
+const defaultResponseBufferSize = 1024
 const defaultResponseMaxSize = 1024 * 1024 * 1024
 
 type maxSizer struct {
@@ -26,7 +27,11 @@ type bodyDuper func(response *http.Response) (body []byte, new io.ReadCloser)
 // copyBody reads the whole response body into a buffer, and returns the slice of bytes from it as well as the reader buffer. It also sets the response body to the new buffer.
 // Warning: changes to the returned byte slice may not be reflected into the response automatically, if it is resliced somewhere. If you're unsure, re-set response.Body to a new buffer from the slice.
 func copyBody(response *http.Response) (body []byte, new io.ReadCloser) {
-	buffer := bytes.NewBuffer(make([]byte, 0, response.ContentLength))
+	responseLen := response.ContentLength
+	if responseLen < 0 {
+		responseLen = defaultResponseBufferSize
+	}
+	buffer := bytes.NewBuffer(make([]byte, 0, responseLen))
 	io.Copy(buffer, response.Body)
 	response.Body.Close()
 
