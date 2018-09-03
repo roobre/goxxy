55010f97566fc8c136823fe7e00927b471031257
Roberto Santalla <roobre@roobre.es>
2018-09-03 14:19:59 +0200

    Tests: Added boilerplate tests

---
+++ goxxy_test.go
@@ -0,0 +1,13 @@
+package goxxy
+
+import "testing"
+
+func TestDemux(t *testing.T) {
+	New()
+	//	TODO
+}
+
+func TestProxy(t *testing.T) {
+	New()
+	//	TODO
+}
---
+++ tests/goxxy_test.go
@@ -0,0 +1,11 @@
+package tests
+
+import (
+	"roob.re/goxxy"
+	"testing"
+)
+
+func TestGoxxy(t *testing.T) {
+	goxxy.New()
+	//	TODO
+}
