e6a3651d1be2622e8f1d0aa42560ceab26519cdc
Roberto Santalla <roobre@roobre.es>
2018-09-03 20:58:28 +0200

    Added a few test edge cases to improve coverage

--- modules/HTMLMangler_test.go
+++ modules/HTMLMangler_test.go
@@ -34,3 +34,34 @@ func TestHTMLMangler(t *testing.T) {
 		t.Error("Partial or not found replacement")
 	}
 }
+
+func TestHTMLManglerEdgeCases(t *testing.T) {
+	linkReplacer := func(doc *goquery.Document) {
+		for _, node := range doc.Find("a").Nodes {
+			for i := range node.Attr {
+				attr := &node.Attr[i]
+
+				if attr.Key == "href" {
+					attr.Val = "https://www.carrierlost.net/"
+				}
+			}
+		}
+	}
+
+	htmlMangler := HTMLMangler{}
+
+	response := tests.GetResponse()
+	response.ContentLength = defaultResponseMaxSize + 2
+	response = htmlMangler.Mangle(response)
+
+	htmlMangler.AddModifier(HTMLModifierFunc(linkReplacer))
+
+	response = htmlMangler.Mangle(response)
+
+	buf := bytes.Buffer{}
+	io.Copy(&buf, response.Body)
+
+	if bytes.Compare(buf.Bytes(), []byte(tests.ResponseHTML)) != 0 {
+		t.Error("Response unmodified despite advertised content length")
+	}
+}
