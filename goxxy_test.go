243e303146ba9d75c4e54806834244fff5f5b18d
Roberto Santalla <roobre@roobre.es>
2018-09-04 17:19:09 +0200

    Added some tests for goxxy: demux

--- goxxy_test.go
+++ goxxy_test.go
@@ -1,8 +1,9 @@
 package goxxy
 
 import (
-	"os"
-	"roob.re/goxxy/modules"
+	"bytes"
+	"net/http"
+	"reflect"
 	"testing"
 )
 
@@ -12,31 +13,72 @@ func TestGoxxy(t *testing.T) {
 }
 
 func TestDemux(t *testing.T) {
+	nilBuffer := &bytes.Buffer{}
+
+	req, err := http.NewRequest(http.MethodGet, "http://google.es", nilBuffer)
+
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
 	// Create a new proxy
 	proxy := New()
-
 	// Add an EchoMangler, which just prints the request being mangled by this proxy
-	proxy.AddMangler(modules.EchoMangler("Parent", os.Stdout))
 
-	// Replace all links with https://www.roobre.es
-	rm := &modules.RegexMangler{}
-	rm.AddBodyRegex(`https?://(?:\w+\.\w+)+/`, "https://www.roobre.es/")
-	proxy.AddMangler(rm)
+	// Test empty
+	if reflect.ValueOf(proxy.demux(req)).Pointer() != reflect.ValueOf(proxy).Pointer() {
+		t.Error("Empty proxy did not match")
+	}
 
 	// Add a new child, with their own matchers and manglers.
 	// Requests are matched in depth, deepest match wins.
 	// A proxy without any Matchers matches anything, but will prioritize their children if they match
 	child1 := proxy.Child()
-	child1.Match(HostMatcher(`(\w+\.)*google(\.\w{2,3})+`))
-	child1.AddMangler(modules.EchoMangler("google anything:", os.Stdout))
+
+	// Now we have a child, so it should match instead
+	if reflect.ValueOf(proxy.demux(req)).Pointer() != reflect.ValueOf(child1).Pointer() {
+		t.Error("Empty child did not match")
+	}
+
+	// Add a matcher to it
+	child1.Match(HostMatcher(`google\..+`))
 
 	child11 := child1.Child()
 	// Multiple Matchers are OR'ed together, if any of them matches, the proxy will mangle this request.
 	child11.Match(HostMatcher(`google.es`))
 	child11.Match(HostMatcher(`google.co.uk`))
-	child11.AddMangler(modules.EchoMangler("google.es:", os.Stdout))
+
+	if reflect.ValueOf(proxy.demux(req)).Pointer() != reflect.ValueOf(child11).Pointer() {
+		t.Error("Level two child did not match")
+	}
+
+	req, _ = http.NewRequest(http.MethodGet, "http://google.com", nilBuffer)
+	if reflect.ValueOf(proxy.demux(req)).Pointer() != reflect.ValueOf(child1).Pointer() {
+		t.Error("Level one child did not match")
+	}
+
+	child12 := child1.Child()
+	// Multiple Matchers are OR'ed together, if any of them matches, the proxy will mangle this request.
+	child12.Match(HostMatcher(`google.nonexistent`))
+
+	req, _ = http.NewRequest(http.MethodGet, "http://google.nonexistent", nilBuffer)
+	if reflect.ValueOf(proxy.demux(req)).Pointer() != reflect.ValueOf(child12).Pointer() {
+		t.Error("Level two child did not match")
+	}
 
 	child2 := proxy.Child()
 	child2.Match(HostMatcher(`(\w+\.)*facebook\.\w{2,3}`))
-	child2.AddMangler(modules.EchoMangler("facebook anything:", os.Stdout))
+
+	req, _ = http.NewRequest(http.MethodGet, "http://facebook.com", nilBuffer)
+	if reflect.ValueOf(proxy.demux(req)).Pointer() != reflect.ValueOf(child2).Pointer() {
+		t.Error("No match for facebook")
+	}
+
+	proxy.Match(HostMatcher("something"))
+
+	if p := proxy.demux(req); reflect.ValueOf(p).Pointer() == reflect.ValueOf(proxy).Pointer() ||
+		reflect.ValueOf(p).Pointer() != 0 {
+		t.Error("Match for something that shouldn't")
+	}
 }
